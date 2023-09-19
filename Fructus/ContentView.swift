ff0117a3ed8f147158db4b86384fc14b03247ff2
NowFloats <prathap.reddy@nowfloats.com>
2023-09-19 12:43:02 +0530

    App Icon and Color sets added

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Prathap Reddy on 19/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
