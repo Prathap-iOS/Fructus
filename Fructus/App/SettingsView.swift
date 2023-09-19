eccf4cc42804875759756d349a2c8d510e28a428
NowFloats <prathap.reddy@nowfloats.com>
2023-09-19 16:51:18 +0530

    Settings View added

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,67 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Prathap Reddy on 19/09/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    // MARK: - SECTION 1
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium and calories.None have cholesterol. Fruits are sources of many essential nutritients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
+                    }
+                    
+                    // MARK: - SECTION 2
+                    
+                    // MARK: - SECTION 2
+                }
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                        }
+                                        
+                )
+                .padding()
+            } //: SCROLL
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREIVEW
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView().preferredColorScheme(.dark)
+    }
+}
